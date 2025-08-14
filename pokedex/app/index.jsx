3aa377a8a318f08fb6d2bf4e2954d387895b2171
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2025-08-14 10:50:08 -0300

    Programando a index e as styles do Card pokemon

---
+++ pokedex/app/index.jsx
@@ -0,0 +1,38 @@
+import {  StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "@/components/Header";
+import { pokemons } from "@/data/pokemons"
+
+export default function Index() {
+  
+  const renderPokemon = ({ item }) => (
+    <Text>{item.nome}</Text>
+  )
+  
+  return (
+    <View style={styles.container}>
+      <Header title="Pokédex" />
+      <FlatList
+        data={pokemons}
+        style={styles.cards}
+        keyExtractor={(pokemon) => pokemon.Numero.toString()}
+        renderItem={ renderPokemon }
+        // initialNumToRender={10}
+        // maxToRenderPerBatch={10}
+        // windowSize={5}
+      />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    flexDirection: "column",
+  },
+  cards: {
+    padding: 15,
+    color: '#000'
+  },
+});
